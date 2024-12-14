62982dd656b98cbfcf7394d36010dfa7b5c4025a
Arseniy2059 <semyashulkovy64@mail.ru>
2024-12-14 20:26:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+api = ''
+
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+kb = InlineKeyboardMarkup()
+
+button3 = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
+button4 = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+
+kb.add(button3)
+kb.add(button4)
 
 
-# Press the green button in the gutter to run the script.
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий товоему здоровью.')
+
+
+@dp.message_handler(text='Рассчитать')
+async def main_menu(message):
+    await message.answer('Введите опцию:', reply_markup=kb)
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call):
+    await call.message.answer('10 x weight (kg) + 6.25 x height (cm) - 5 x Age (g) - 161')
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    executor.start_polling(dp, skip_updates=True)
